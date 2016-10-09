a16d25d00287d53ba669835ce74ce0cc6c3c1f6e
Mike Liu <mikelkl@foxmail.com>
2016-10-09 12:01:02 +0800

    migrate to vue.js 2.0

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,5 +1,7 @@
+var config = require('../config')
 var webpack = require('webpack')
 var merge = require('webpack-merge')
+var utils = require('./utils')
 var baseWebpackConfig = require('./webpack.base.conf')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
@@ -9,9 +11,15 @@ Object.keys(baseWebpackConfig.entry).forEach(function (name) {
 })
 
 module.exports = merge(baseWebpackConfig, {
+  module: {
+    loaders: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
+  },
   // eval-source-map is faster for development
   devtool: '#eval-source-map',
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': config.dev.env
+    }),
     // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
@@ -21,10 +29,6 @@ module.exports = merge(baseWebpackConfig, {
       filename: 'index.html',
       template: 'index.html',
       inject: true
-    }),
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
     })
   ]
 })
--- src/components/LogTime.vue
+++ src/components/LogTime.vue
@@ -1,73 +1,53 @@
+// src/components/LogTime.vue
+
 <template>
   <div class="form-horizontal">
     <div class="form-group">
       <div class="col-sm-6">
         <label>日期</label>
-        <input
-          type="date"
-          class="form-control"
-          v-model="timeEntry.date"
-          placeholder="日期"
-        />
+        <input type="date" class="form-control" v-model="timeEntry.date" placeholder="Date" />
       </div>
       <div class="col-sm-6">
-        <label>所需时间</label>
-        <input
-          type="number"
-          class="form-control"
-          v-model="timeEntry.totalTime"
-          placeholder="所需时间"
-        />
+        <label>时间</label>
+        <input type="number" class="form-control" v-model="timeEntry.totalTime" placeholder="Hours" />
       </div>
     </div>
     <div class="form-group">
       <div class="col-sm-12">
         <label>备注</label>
-        <input
-          type="text"
-          class="form-control"
-          v-model="timeEntry.comment"
-          placeholder="备注"
-        />
+        <input type="text" class="form-control" v-model="timeEntry.comment" placeholder="Comment" />
       </div>
     </div>
     <button class="btn btn-primary" @click="save()">保存</button>
-    <button v-link="'/time-entries'" class="btn btn-danger">取消</button>
+    <!-- <button v-link="'/time-entries'" class="btn btn-danger">取消</button> -->
+    <router-link to="/time-entries"><button v-link="'/time-entries'" class="btn btn-danger">取消</button></router-link>
     <hr>
   </div>
+
 </template>
-<style>
 
-</style>
 <script>
-
-    export default{
-      data () {
-        return {
-          timeEntry: {
-            /*user: {
-              name: '二哲',
-              email: 'kodo@forchange.cn',
-              image: 'https://sfault-avatar.b0.upaiyun.com/888/223/888223038-5646dbc28d530_huge256'
-            },*/
-          }
-        }
-      },
-      methods: {
-        save () {
-          this.$http.post('http://localhost:8888/create',{
-            comment : this.timeEntry.comment,
-            totalTime : this.timeEntry.totalTime,
-            date : this.timeEntry.date
-          }).then(function(ret) {
-            console.log(ret);
-            let timeEntry = this.timeEntry
-            console.log(timeEntry);
-            this.$dispatch('timeUpdate', timeEntry)
-            this.timeEntry = {}
-          })
-
-        }
+  export default {
+    data () {
+      return {
+        timeEntry: {}
+      }
+    },
+    methods: {
+      save () {
+        this.$http.post('http://localhost:8888/create', {
+          comment: this.timeEntry.comment,
+          totalTime: this.timeEntry.totalTime,
+          date: this.timeEntry.date
+        }).then(function (ret) {
+          console.log(ret)
+          let timeEntry = this.timeEntry
+          console.log(timeEntry)
+          // this.$dispatch('timeUpdate', timeEntry)
+          this.$store.commit('timeUpdate', timeEntry)
+          this.timeEntry = {}
+        })
       }
     }
+  }
 </script>
